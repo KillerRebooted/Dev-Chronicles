1dc3e856c856be2f179d88084a45f624daee9c10
KillerRebooted <killerrebooted@gmail.com>
2025-09-17 16:51:20 +0530

    C practice

--- C/Test/test.c
+++ C/Test/test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <ctype.h>
 
 void main() {
-    float a;
-    scanf("%f", &a);
-    printf("%f\n", a);
+    int N=10, sum=0;
+    for (; N>=1; N--) {
+        printf("%d, %d\n", N, sum);
+        sum += N;
+    }
+    printf("Sum of Numbers upto %d is %d", N, sum);
+
+    /*N = 6;
+    int factorial = 1;
+    do {
+        factorial *= N;
+        N--;
+    } while (N>1);
+
+    printf("%d\n", factorial);
+    */
+
 }
--- Python/Programs/Miscellaneous Codes/Spiral Printing.py
+++ Python/Programs/Miscellaneous Codes/Spiral Printing.py
@@ -2,27 +2,27 @@ import math
 
 n = int(input("Enter the Size of Spiral: "))
 
-spiral = [[0]*n]
-[spiral.append([0]*n) for _ in range(n-1)]
+spiral = [[0]*n for _ in range(n)]
 
 i = 0
 j = 0
 
 directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
-direction = 0
+direction = -1
+
+i_add, j_add = -1, -1
 
 for num in range(1, n**2+1):
     spiral[i][j] = num
 
-    i_add = directions[direction][0]
-    j_add = directions[direction][1]
-
     if (i+i_add not in range(0, n)) or (j+j_add not in range(0, n)) or (spiral[i+i_add][j+j_add]):
         direction += 1
         direction %= 4
+        i_add = directions[direction][0]
+        j_add = directions[direction][1]
     
-    i += directions[direction][0]
-    j += directions[direction][1]
+    i += i_add
+    j += j_add
 
 for i in spiral:
     for j in i:
